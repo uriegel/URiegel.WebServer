4d8eb02619c3aa214110f491a114b41b341794fa
Uwe Riegel <uwriegel@gmail.com>
2017-11-08 21:06:53 +0100

    Restart problem linux

--- WebServer/server.fs
+++ WebServer/server.fs
@@ -41,6 +41,8 @@ let rec beginConnect (listener: TcpListener) configuration =
 let private start (listener: TcpListener, configuration: Configuration.Value) () = 
     try
         printfn "Starting HTTP Listener..."
+        // Ansonsten kann nach Beenden des Listeners für 2 min kein neuer gestartet werden!
+        listener.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)    
         listener.Start ()
         beginConnect listener configuration
         printfn "HTTP Listener started"
