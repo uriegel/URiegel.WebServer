e51ff685705568092a03bc8a773b1d380025ba99
Uwe Riegel <uwriegel@gmail.com>
2017-12-13 23:38:23 +0100

    Request with query

--- WebServer/types/url-query-components.fs
+++ WebServer/types/url-query-components.fs
@@ -12,20 +12,22 @@ type Query =
     member this.Query key = this.rawParameters.TryFind key
 
 let create (url: string) = 
-    let pos = url.LastIndexOf '/' + 1 
     if url.Contains "?" then
         let urlParameterString = Uri.UnescapeDataString url
         let matches = urlParameterRegex.Matches urlParameterString
         
         let unescapeSpaces (uri: string) = uri.Replace ('+', ' ')
+        let methodPath = url.Substring (0, url.IndexOf('?'))
+        let pos = methodPath.LastIndexOf '/' + 1 
         {
-            method = url.Substring (pos, url.IndexOf ('?') - pos)
+            method = methodPath.Substring pos
             rawParameters = matches 
                             |> Seq.cast 
                             |> Seq.map (fun (s: Match) -> (s.Groups.["key"].Value, Uri.UnescapeDataString (unescapeSpaces s.Groups.["value"].Value))) 
                             |> Map.ofSeq
         }
     else 
+        let pos = url.LastIndexOf '/' + 1 
         {
             method = url.Substring (pos)
             rawParameters = Map.empty
