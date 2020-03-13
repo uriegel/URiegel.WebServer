2c1aae5134894bce35bc707a1d1fff0a7a87d69c
Uwe Riegel <uriegel@hotmail.de>
2020-03-13 08:11:23 +0100

    LetsEncrypt creator

---
+++ LetsEncrypt/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Certes;
+using Certes.Acme;
+using Certes.Acme.Resource;
+
+namespace LetsEncrypt
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AcmeContext acme = null;
+            IAccountContext account = null;
+            if (File.Exists("account.txt"))
+            {
+                var pemKey = await File.ReadAllTextAsync("account.txt");
+                var accountKey = KeyFactory.FromPem(pemKey);
+                acme = new AcmeContext(WellKnownServers.LetsEncryptStagingV2, accountKey);
+                account = await acme.Account();                
+            }
+            else
+            {
+                acme = new AcmeContext(WellKnownServers.LetsEncryptStagingV2);
+                account = await acme.NewAccount("uriegel@hotmail.de", true);
+                //account = await acme.NewAccount("uwriegel@gmail.com", true);
+                // Save the account key for later use
+                var pemKey = acme.AccountKey.ToPem();
+                await File.WriteAllTextAsync("account.txt", pemKey);
+            }
+            var order = await acme.NewOrder(new[] { "uriegel.de" });
+            //var order = await acme.NewOrder(new[] { "fritz.uriegel.de" });
+
+            var authz = (await order.Authorizations()).First();
+            var httpChallenge = await authz.Http();
+            var keyAuthz = httpChallenge.KeyAuthz;
+            var token = httpChallenge.Token;
+
+            await File.WriteAllTextAsync("webroot/letsencrypt", keyAuthz);
+
+            var challenge = await httpChallenge.Validate();
+
+            var test = 0;
+            
+        }
+    }
+}
--- Tester/Program.fs
+++ Tester/Program.fs
@@ -2,6 +2,7 @@
 open Configuration
 open Session
 open Request
+open System.IO
 
 type Command = {
     Cmd: string
@@ -14,6 +15,8 @@ type Input = {
     name: string
 }
 
+System.IO.Directory.SetCurrentDirectory "/media/speicher/projekte/UwebServer"
+
 // TODO:
 // Testprogramm: TCPSender: send Header, dann Send Json
 // Testprogramm: dann SendJson > 20000 bzw. Buffersize = 1000 Json > 4000
@@ -23,11 +26,19 @@ printfn "Starting Test Server"
 let asyncRequest (requestSession: RequestSession) = 
     async {
         let request = requestSession.Query.Value
-        match requestSession.Query.Value.Request with
-        | "login" -> 
+        
+        let p = 
+            match request.Path with
+            | Some p -> p
+            | None -> "nix"
+
+        printfn "%s %s" p requestSession.Query.Value.Request
+
+        match requestSession.Query.Value.Request, request.Path with
+        | "login", _ -> 
             let data = requestSession.GetText ()
             return false
-        | "runOperation" ->
+        | "runOperation", _ ->
             let jason = getJson<Input> requestSession 
             let id = jason.id
             let command = {
@@ -38,7 +49,7 @@ let asyncRequest (requestSession: RequestSession) =
             //System.Threading.Thread.Sleep 3
             do! requestSession.AsyncSendJson (command :> obj)
             return true
-        | "affe" ->
+        | "affe", _ ->
             let test = requestSession.Query.Value
             let param1 = test.Query "param1" 
             let param2 = test.Query "param2"
@@ -51,6 +62,12 @@ let asyncRequest (requestSession: RequestSession) =
             }
             //System.Threading.Thread.Sleep 3
             do! requestSession.AsyncSendJson (command :> obj)
+            return true
+        | _, Some ".well-known/acme-challenge" ->
+            let token = File.ReadAllText("webroot/letsencrypt")
+            printfn "Gib token : %s Token: %s" request.Request token
+            //let token = token |> String.substring ((token |> String.indexOfChar '.').Value + 1)
+            do! requestSession.AsyncSendText token
             return true
         | _ -> return false
     }
@@ -67,8 +84,8 @@ let onNewWebSocket _ __ =
 let configuration = Configuration.create {
     Configuration.createEmpty() with 
         WebRoot = "/media/speicher/projekte/UwebServer/webroot" 
-        Port = 9865
-        TlsPort = 443
+        // Port = 9865
+        // TlsPort = 4434
         UseLetsEncrypt = true
         AllowOrigins = Some [| "http://localhost:8080" |]
         onNewWebSocket = onNewWebSocket
