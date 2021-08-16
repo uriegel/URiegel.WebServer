d62524bc0370e4c9c3da47be89d90b6ccc446f95
uriegel <uriegel@hotmail.de>
2021-08-16 17:57:14 +0200

    Converted to C#

---
+++ Tester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UwebServer;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- webroot/websockets/main.js
+++
@@ -1,2 +0,0 @@
-var socke = new WebSocket("ws://localhost:20000/Socke")
-socke.onopen = e => alert("Bin offen")
